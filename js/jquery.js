0ac6f5f10c3aba9d460d7cec97a635b5ad4ed6b6
Brandon G-Home <brandon.gardner2@outlook.com>
2019-02-04 17:40:05 -0600

    Animations base done

--- js/jquery.js
+++ js/jquery.js
@@ -20,3 +20,27 @@ $(".js-wp-1").waypoint(
     offset: "50%;"
   }
 );
+$(".js-wp-2").waypoint(
+  function(direction) {
+    $(".js-wp-2").addClass("animated fadeInUp");
+  },
+  {
+    offset: "50%;"
+  }
+);
+$(".js-wp-3").waypoint(
+  function(direction) {
+    $(".js-wp-3").addClass("animated slideInLeft");
+  },
+  {
+    offset: "50%;"
+  }
+);
+$(".js-wp-4").waypoint(
+  function(direction) {
+    $(".js-wp-4").addClass("animated bounceInUp");
+  },
+  {
+    offset: "50%;"
+  }
+);
