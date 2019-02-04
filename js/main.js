cd18a933a98b4138a8ec0753964ec35c0a6d97cb
Brandon G-Home <brandon.gardner2@outlook.com>
2019-02-04 11:11:27 -0600

    Nav modal finished

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const modal = document.querySelector(".nav-container");
+const modalBtn = document.querySelector(".menu-icon");
+const modalCloseBtn = document.querySelector(".close-menu");
+
+modalBtn.onclick = () => {
+  modal.style.display = "block";
+};
+
+modalCloseBtn.onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = e => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
