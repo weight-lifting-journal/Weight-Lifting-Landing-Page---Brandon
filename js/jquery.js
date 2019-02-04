113ebb0015f03225444df1b5fc7fb4d6ca381cdc
Brandon G-Home <brandon.gardner2@outlook.com>
2019-02-04 16:04:44 -0600

    Smooth scroll nav

---
+++ js/jquery.js
@@ -0,0 +1,12 @@
+/*Scroll on Nav*/
+$('a[href*="#"]').on("click", function(e) {
+  e.preventDefault();
+
+  $("html, body").animate(
+    {
+      scrollTop: $($(this).attr("href")).offset().top
+    },
+    1500,
+    "linear"
+  );
+});
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*------------Modal Functionality------------*/
 const modal = document.querySelector(".nav-container");
 const modalBtn = document.querySelector(".menu-icon");
 const modalCloseBtn = document.querySelector(".close-menu");
@@ -15,4 +16,34 @@ window.onclick = e => {
   if (e.target === modal) {
     modal.style.display = "none";
   }
+};
+
+/*------------Nav Functionality------------*/
+const homeBtn = document.querySelector("#homeBtn");
+const home = document.querySelector("#home");
+homeBtn.onclick = () => {
+  window.scroll({ top: home.offsetTop, behavior: "smooth" });
+  modal.style.display = "none";
+};
+
+const featuresBtn = document.querySelector("#featuresBtn");
+const features = document.querySelector("#features");
+featuresBtn.onclick = () => {
+  features.scrollIntoView();
+  // window.scroll({ top: features.offsetTop, behavior: "smooth" });
+  modal.style.display = "none";
+};
+
+const contactBtn = document.querySelector("#contactBtn");
+const contact = document.querySelector("#contact");
+contactBtn.onclick = () => {
+  window.scroll({ top: contact.offsetTop, behavior: "smooth" });
+  modal.style.display = "none";
+};
+
+const testimonialsBtn = document.querySelector("#testimonialsBtn");
+const testimonials = document.querySelector("#testimonials");
+testimonialsBtn.onclick = () => {
+  window.scroll({ top: testimonials.offsetTop, behavior: "smooth" });
+  modal.style.display = "none";
 };
